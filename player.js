359ff90994fbf16ae4dc872c941a9b794a7b2c6f
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-03-05 12:05:26 -0700

    Add choose your fighter functionality to populate to dom

--- player.js
+++ player.js
@@ -1,7 +1,12 @@
 class Player {
-  constructor(name) {
+  constructor(name, choice, game) {
     this.name = name;
     this.wins = 0;
+    if (this.name === "playerOne") {
+      this.choice = choice;
+    // } else if (this.name === "computer") {
+      // this.choice = game.type[getRandomIndex(game.type)];
+    }
   }
   // savesWinsToStorage() {
   //   // increment win based on the outcome of the game
@@ -11,11 +16,6 @@ class Player {
   //   // timeout feature ???
   // }
   takeTurn(choice, game) {
-    if (this.name === "playerOne") {
-    this.choice = choice;
-  } else if (this.name === "computer") {
-    this.choice = game.type[getRandomIndex(game.type)];
-  }
     // be invoked in the choose your fighter screen when one of the icons is clicked. (player one)
     // be invoked to choose a random option based on that the name of the player is.
     //conditionals here.
